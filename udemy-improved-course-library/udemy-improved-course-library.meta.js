86def83cdb309c93771ed3693aaa84841c63ccd1
Tad Wohlrapp <tadwohlrapp@gmail.com>
2020-05-06 20:55:03 +0200

    Testing namespace change impact on update functionality

--- udemy-improved-course-library/udemy-improved-course-library.meta.js
+++ udemy-improved-course-library/udemy-improved-course-library.meta.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Udemy - Improved Course Library
-// @namespace    https://github.com/TadWohlrapp/UserScripts
+// @namespace    https://github.com/TadWohlrapp/UserScripts_foo
 // @description  Shows rating, number of reviews and number of students enrolled for all courses in your library
 // @icon         https://github.com/TadWohlrapp/UserScripts/raw/master/udemy-improved-course-library/icon.png
 // @icon64       https://github.com/TadWohlrapp/UserScripts/raw/master/udemy-improved-course-library/icon64.png
 // @author       Tad Wohlrapp <tadwohlrapp@gmail.com>
 // @homepageURL  https://github.com/TadWohlrapp/UserScripts/udemy-improved-course-library
-// @version      0.0.1
+// @version      0.0.2
 // @updateURL    https://github.com/TadWohlrapp/UserScripts/raw/master/udemy-improved-course-library/udemy-improved-course-library.meta.js
 // @downloadURL  https://github.com/TadWohlrapp/UserScripts/raw/master/udemy-improved-course-library/udemy-improved-course-library.user.js
 // @match        https://www.udemy.com/home/my-courses/*
