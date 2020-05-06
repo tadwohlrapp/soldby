ebd36847966ad20957798161eded21c0ffd93d82
Tad Wohlrapp <tadwohlrapp@gmail.com>
2020-05-06 20:59:16 +0200

    test supportURL

--- udemy-improved-course-library/udemy-improved-course-library.user.js
+++ udemy-improved-course-library/udemy-improved-course-library.user.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @name         Udemy - Improved Course Library
-// @namespace    https://github.com/TadWohlrapp/UserScripts_foo
+// @namespace    https://github.com/TadWohlrapp/UserScripts
 // @description  Shows rating, number of reviews and number of students enrolled for all courses in your library
 // @icon         https://github.com/TadWohlrapp/UserScripts/raw/master/udemy-improved-course-library/icon.png
 // @icon64       https://github.com/TadWohlrapp/UserScripts/raw/master/udemy-improved-course-library/icon64.png
 // @author       Tad Wohlrapp <tadwohlrapp@gmail.com>
 // @homepageURL  https://github.com/TadWohlrapp/UserScripts/udemy-improved-course-library
-// @version      0.0.2
+// @version      0.0.3
 // @updateURL    https://github.com/TadWohlrapp/UserScripts/raw/master/udemy-improved-course-library/udemy-improved-course-library.meta.js
 // @downloadURL  https://github.com/TadWohlrapp/UserScripts/raw/master/udemy-improved-course-library/udemy-improved-course-library.user.js
+// @supportURL   https://github.com/TadWohlrapp/UserScripts/issues
 // @match        https://www.udemy.com/home/my-courses/*
 // @copyright    2020, Tad Wohlrapp (https://github.com/TadWohlrapp/UserScripts)
 // @license      MIT
