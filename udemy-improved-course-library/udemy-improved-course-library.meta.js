9f8e99a2eedc7e5ee432f408b6d4e0e2e90af2da
Tad Wohlrapp <tadwohlrapp@gmail.com>
2020-05-07 18:48:06 +0200

    Add @compatible meta tag

--- udemy-improved-course-library/udemy-improved-course-library.meta.js
+++ udemy-improved-course-library/udemy-improved-course-library.meta.js
@@ -6,11 +6,13 @@
 // @icon64       https://github.com/TadWohlrapp/UserScripts/raw/master/udemy-improved-course-library/icon64.png
 // @author       Tad Wohlrapp <tadwohlrapp@gmail.com>
 // @homepageURL  https://github.com/TadWohlrapp/UserScripts/tree/master/udemy-improved-course-library
-// @version      0.3.3
+// @version      0.3.4
 // @updateURL    https://github.com/TadWohlrapp/UserScripts/raw/master/udemy-improved-course-library/udemy-improved-course-library.meta.js
 // @downloadURL  https://github.com/TadWohlrapp/UserScripts/raw/master/udemy-improved-course-library/udemy-improved-course-library.user.js
 // @supportURL   https://github.com/TadWohlrapp/UserScripts/issues
 // @match        https://www.udemy.com/home/my-courses/*
+// @compatible   chrome Tested with Tampermonkey v4.9 and Violentmonkey v2.12.7
+// @compatible   firefox Tested with Greasemonkey v4.9
 // @copyright    2020, Tad Wohlrapp (https://github.com/TadWohlrapp/UserScripts)
 // @license      MIT
 // ==/UserScript==
