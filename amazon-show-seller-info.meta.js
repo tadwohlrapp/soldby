0df3f88974807b5d5b79e9d5fd9b55320f8e1d35
Tad Wohlrapp <git@wohlr.app>
2021-10-25 16:57:00 +0200

    Update meta infos after recent repo renaming

--- amazon-show-seller-info.meta.js
+++ amazon-show-seller-info.meta.js
@@ -4,20 +4,21 @@
 // @name:fr         Amazon - Afficher les informations sur le vendeur
 // @name:es         Amazon - Mostrar información del vendedor
 // @name:it         Amazon - Mostra info venditore
-// @namespace       https://github.com/TadWohlrapp/UserScripts
 // @description     Shows name, country of origin and ratings for third party sellers on Amazon (and highlights Chinese sellers)
 // @description:de  Zeigt Name, Herkunftsland und Bewertungen von Drittanbietern auf Amazon (und hebt chinesische Anbieter hervor)
 // @description:fr  Montre le nom, le pays d'origine et les évaluations des vendeurs tiers sur Amazon (et met en évidence les vendeurs chinois)
 // @description:es  Muestra el nombre, el país de origen y las valoraciones de los vendedores de terceros en el Amazon (y destaca los vendedores chinos)
 // @description:it  Mostra il nome, il paese di origine e le valutazioni per i venditori di terze parti su Amazon (e mette in evidenza i venditori cinesi)
-// @icon            https://github.com/TadWohlrapp/UserScripts/raw/master/amazon-show-seller-info/icon.png
-// @icon64          https://github.com/TadWohlrapp/UserScripts/raw/master/amazon-show-seller-info/icon64.png
-// @author          Tad Wohlrapp <tadwohlrapp@gmail.com>
-// @homepageURL     https://github.com/TadWohlrapp/UserScripts/tree/master/amazon-show-seller-info
+// @namespace       https://github.com/tadwohlrapp
+// @author          Tad Wohlrapp
 // @version         1.1.3
-// @updateURL       https://github.com/TadWohlrapp/UserScripts/raw/master/amazon-show-seller-info/amazon-show-seller-info.meta.js
-// @downloadURL     https://github.com/TadWohlrapp/UserScripts/raw/master/amazon-show-seller-info/amazon-show-seller-info.user.js
-// @supportURL      https://github.com/TadWohlrapp/UserScripts/issues
+// @license         MIT
+// @homepageURL     https://github.com/tadwohlrapp/amazon-show-seller-info-userscript
+// @supportURL      https://github.com/tadwohlrapp/amazon-show-seller-info-userscript/issues
+// @updateURL       https://github.com/tadwohlrapp/amazon-show-seller-info-userscript/raw/main/amazon-show-seller-info.meta.js
+// @downloadURL     https://github.com/tadwohlrapp/amazon-show-seller-info-userscript/raw/main/amazon-show-seller-info.user.js
+// @icon            https://github.com/tadwohlrapp/amazon-show-seller-info-userscript/raw/main/icon.png
+// @icon64          https://github.com/tadwohlrapp/amazon-show-seller-info-userscript/raw/main/icon64.png
 // @match           https://smile.amazon.co.uk/*
 // @match           https://www.amazon.co.uk/*
 // @match           https://smile.amazon.de/*
@@ -25,12 +26,6 @@
 // @match           https://www.amazon.es/*
 // @match           https://www.amazon.fr/*
 // @match           https://www.amazon.it/*
-// @compatible      chrome Tested with Tampermonkey v4.9 and Violentmonkey v2.12.7
-// @compatible      firefox Tested with Greasemonkey v4.9
-// @copyright       2020, Tad Wohlrapp (https://github.com/TadWohlrapp/UserScripts)
-// @license         MIT
+// @compatible      firefox Tested on Firefox v90 with Violentmonkey v2.13.0, Tampermonkey v4.13 and Greasemonkey v4.11
+// @compatible      chrome Tested on Chrome v94  with Violentmonkey v2.13.0 and Tampermonkey v4.13
 // ==/UserScript==
-
-// ==OpenUserJS==
-// @author Taddiboy
-// ==/OpenUserJS==
